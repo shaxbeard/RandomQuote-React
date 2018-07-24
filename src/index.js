d6e3eeb0beb562c95a3a195911c25e2351d183dc
Eric Carlson <eric.carlson2972@gmail.com>
2018-07-24 09:54:29 -0400

    Quotes sent to console

--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/card';
 
+var quoteFull;	
+var quoteArray = [];
+var authorArray = [];
+var i = -1;
+
+(function () { 
+  quoteFull = $.ajax({
+    url: 'https://talaikis.com/api/quotes/random/',
+    type: 'GET', 
+    dataType: 'json',
+    success: function(data) {
+      quoteArray.push(data.quote);
+      authorArray.push(data.author);
+      i+=1; 
+      console.log(i);
+      console.log(quoteArray[i]);
+    //   document.getElementById("output").innerHTML = quoteArray[i];
+    //   document.getElementById("author").innerHTML = authorArray[i];
+        }
+}); // AJAX method ??
+})(); // doIt Function
+
+
+
 // Create a new component. This //component should produce
 // some HTML
 
