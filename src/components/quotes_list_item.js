18194a10b919e485ab9a0d0e96cca6f5704a01c2
Eric Carlson <eric.carlson2972@gmail.com>
2018-08-01 22:54:42 -0400

    02-09-list-items-with-photo-and-email

--- src/components/quotes_list_item.js
+++ src/components/quotes_list_item.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const QuotesListItem = (props) => {
-    return <li>Quote</li>
-};
+const QuotesListItem = ({quote}) => {
+    const imageUrl = quote.picture.thumbnail;
+    return(
+        <li className="list-group-item">
+        <div className="quote-list media">
+          <div className="media-left">
+            <img className="media-object" src= {imageUrl} />
+          </div>
+        
+        <div className="media-body">
+          <div className="media-heading">
+          {quote.email}
+          
+         </div>
+        
+        
+        
+        
+        </div>
+  
+        </div>
+        </li>
+      );
+  };
+  
 
 export default QuotesListItem;
